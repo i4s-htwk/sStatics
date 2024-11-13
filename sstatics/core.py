0668bc6ba2fc5914619efb41c0fe2f9efe52846f
pbrassel <paul.brassel@htwk-leipzig.de>
2024-11-13 17:08:21 +0100

    validate Material

--- sstatics/core.py
+++ sstatics/core.py
@@ -100,7 +100,6 @@ class CrossSection:
             )
 
 
-# validierung
 @dataclass(eq=False)
 class Material:
 
@@ -108,6 +107,16 @@ class Material:
     poisson: float
     shear_mod: float
     therm_exp_coeff: float
+
+    def __post_init__(self):
+        if self.young_mod <= 0:
+            raise ValueError('young_mod has to be greater than zero.')
+        if self.poisson <= 0:
+            raise ValueError('poisson has to be greater than zero.')
+        if self.shear_mod <= 0:
+            raise ValueError('shear_mod has to be greater than zero.')
+        if self.therm_exp_coeff <= 0:
+            raise ValueError('therm_exp_coeff has to be greater than zero.')
 
 
 # pi, pj validierung?
