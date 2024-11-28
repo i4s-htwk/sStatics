8248009abd57983ceccae8510506f26025510421
pbrassel <paul.brassel@htwk-leipzig.de>
2024-11-28 14:54:29 +0100

    import model classes at top level and set __all__ for safe star imports

---
+++ sstatics/__init__.py
@@ -0,0 +1,19 @@
+
+from sstatics.core import (
+    Bar, BarLineLoad, BarPointLoad, BarTemp, CrossSection, Material, Model,
+    Node, NodeDisplacement, NodePointLoad, System
+)
+
+__all__ = [
+    'Bar',
+    'BarLineLoad',
+    'BarPointLoad',
+    'BarTemp',
+    'CrossSection',
+    'Material',
+    'Model',
+    'Node',
+    'NodeDisplacement',
+    'NodePointLoad',
+    'System',
+]
