72f20cc50e4a5545396879d54064c89cea424363
Clarissa <clarissa_sophie.scholle@stud.htwk-leipzig.de>
2025-05-23 15:53:47 +0200

    refactor: integrate transform function, SingleGraphicObject and class methods from Line

--- sstatics/graphic_objects/hinges.py
+++ sstatics/graphic_objects/hinges.py
@@ -1,18 +1,41 @@
 
 import numpy as np
+from abc import ABC
 import plotly.graph_objs as go
+
 from sstatics.graphic_objects import (
-    rotate, GraphicObject, Line, Ellipse
+    transform, SingleGraphicObject, Line, Ellipse
 )
 
 
 class Hinge(GraphicObject):
+class Hinge(SingleGraphicObject, ABC):
 
     def __init__(self, x, z, width, **kwargs):
         if width <= 0:
             raise ValueError('"width" has to be a number greater than zero.')
         super().__init__(x, z, **kwargs)
         self.width = width * self.scale
+
+
+class NormalForceHinge(Hinge):
+
+    def __init__(self, x, z, width=11/30, **kwargs):
+        super().__init__(x, z, width, **kwargs)
+
+    @property
+    def traces(self):
+        x_off, z_off = 11 / 20, self.width / 2
+        x = np.array(
+            [self.x + 3 / 4 * x_off, -1 / 4 * x_off + self.x,
+             -1 / 4 * x_off + self.x, self.x + 3 / 4 * x_off]
+        )
+        z = np.array([
+            self.z - z_off, self.z - z_off,
+            self.z + z_off, self.z + z_off
+        ])
+        x, z = transform(self.x, self.z, x, z, self.rotation, self.scale)
+        return go.Scatter(x=x, y=z, **self.scatter_kwargs),
 
 
 class ShearForceHinge(Hinge):
@@ -22,40 +45,22 @@ class ShearForceHinge(Hinge):
 
     @property
     def traces(self):
-        left_line = Line(
-            self.x - self.width / 2, self.z, 11 / 20,
-            rotation=np.pi / 2, scale=self.scale, **self.scatter_kwargs
+        length, x_offset = 11 / 20, self.width / 2
+        left_line = Line.from_center(
+            self.x - x_offset, self.z, length,
+            rotation=np.pi / 2, **self.scatter_kwargs
         )
-        left_line_traces = left_line.rotate_traces(
-            self.x, self.z, self.rotation
+        left_line_traces = left_line.transform_traces(
+            self.x, self.z, self.rotation, self.scale
         )
-        right_line = Line(
-            self.x + self.width / 2, self.z, 11 / 20,
-            rotation=np.pi / 2, scale=self.scale, **self.scatter_kwargs
+        right_line = Line.from_center(
+            self.x + x_offset, self.z, length,
+            rotation=np.pi / 2, **self.scatter_kwargs
         )
-        right_line_traces = right_line.rotate_traces(
-            self.x, self.z, self.rotation
+        right_line_traces = right_line.transform_traces(
+            self.x, self.z, self.rotation, self.scale
         )
         return *left_line_traces, *right_line_traces
-
-
-class NormalForceHinge(Hinge):
-
-    def __init__(self, x, z, width=11/30, **kwargs):
-        super().__init__(x, z, width=width, **kwargs)
-
-    @property
-    def traces(self):
-        x = np.array([
-            self.x + self.scale * 11 / 20, self.x,
-            self.x, self.x + self.scale * 11 / 20
-        ])
-        z = np.array([
-            self.z - self.width / 2, self.z - self.width / 2,
-            self.z + self.width / 2, self.z + self.width / 2
-        ])
-        x, z = rotate(self.x, self.z, x, z, self.rotation)
-        return go.Scatter(x=x, y=z, **self.scatter_kwargs),
 
 
 class MomentHinge(Hinge, Ellipse):
