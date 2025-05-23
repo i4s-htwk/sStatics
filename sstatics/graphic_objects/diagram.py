d6c8333857b7f8e2694b7b1b41011811635624d8
Clarissa <clarissa_sophie.scholle@stud.htwk-leipzig.de>
2025-05-23 15:52:16 +0200

    refactor: integrate transform function, SingleGraphicObject and class methods from Line and IsoscelesTriangle

--- sstatics/graphic_objects/diagram.py
+++ sstatics/graphic_objects/diagram.py
@@ -1,15 +1,17 @@
-# arrow, coordinatesystem, hatching
 
 import numpy as np
 import plotly.graph_objs as go
+
 from sstatics.graphic_objects import (
-    rotate, GraphicObject, Line, IsoscelesTriangle, Rectangle
+    rotate, transform, SingleGraphicObject, Line, IsoscelesTriangle, Rectangle
 )
 
 
-class Arrow(GraphicObject):
+class Arrow(SingleGraphicObject):
+    # TODO: change intern scaling for arrow
+    # TODO: better rotation
 
-    scatter_options = IsoscelesTriangle.scatter_options | {
+    scatter_options = SingleGraphicObject.scatter_options | {
         'fill': 'toself',
         'fillcolor': 'black',
     }
@@ -24,21 +26,21 @@ class Arrow(GraphicObject):
 
     @property
     def traces(self):
-        arrow = IsoscelesTriangle(
+        arrow = IsoscelesTriangle.from_angle(
             self.x, self.z, rotation=np.pi,
             scale=self.scale / (1 + self.tail_length), **self.scatter_kwargs
         )
-        arrow_traces = arrow.rotate_traces(self.x, self.z, self.rotation)
+        arrow_traces = arrow.transform_traces(self.x, self.z, self.rotation)
         z_offset = np.cos(np.pi / 8) * self.scale / (1 + self.tail_length)
-        tail = Line(
-            self.x, self.z - (self.scale + z_offset) / 2,
-            self.scale - z_offset, rotation=np.pi / 2, **self.scatter_kwargs
+        tail = Line.from_points(
+            [(self.x, self.z - z_offset), (self.x, self.z - self.scale)],
+            **self.scatter_kwargs
         )
-        tail_traces = tail.rotate_traces(self.x, self.z, self.rotation)
+        tail_traces = tail.transform_traces(self.x, self.z, self.rotation)
         return *arrow_traces, *tail_traces
 
 
-class CoordinateSystem(GraphicObject):
+class CoordinateSystem(SingleGraphicObject):
 
     def __init__(self, x, z, x_text=None, z_text=None, **kwargs):
         super().__init__(x, z, **kwargs)
@@ -79,15 +81,19 @@ class CoordinateSystem(GraphicObject):
         z_axis = Arrow(
             self.x, self.z + self.scale, tail_length=6, scale=self.scale
         )
-        x_axis_traces = x_axis.rotate_traces(self.x, self.z, self.rotation)
-        z_axis_traces = z_axis.rotate_traces(self.x, self.z, self.rotation)
-        return x_axis_traces + z_axis_traces
+        x_axis_traces = x_axis.transform_traces(
+            self.x, self.z, self.rotation, self.scale
+        )
+        z_axis_traces = z_axis.transform_traces(
+            self.x, self.z, self.rotation, self.scale
+        )
+        return *x_axis_traces, *z_axis_traces
 
 
 class Hatching(Rectangle):
 
     def __init__(
-            self, x, z, a, b=None, angle=np.pi / 4, spacing=0.17,
+            self, x, z, a, b=None, angle=np.pi / 4, spacing=0.2,
             rectangle=False, **kwargs
     ):
         if not 0 < angle < np.pi:
@@ -96,40 +102,40 @@ class Hatching(Rectangle):
             )
         super().__init__(x, z, a, b, **kwargs)
         self.angle = angle
-        self.spacing = spacing * self.scale
+        self.spacing = spacing
         self.rectangle = rectangle
 
     @property
     def traces(self):
-        x_offset, z_offset = self.a / 2, self.b / 2
+        x_off, z_off = self.a / 2, self.b / 2
         tan_angle = np.tan(self.angle)
         rect = super().traces[0] if self.rectangle else go.Scatter(x=[], y=[])
         x_list, z_list = [], []
-        n = 1
+        n = 1 / 2
         max_dim = max(self.a, self.b)
         while (d := np.sqrt((n * self.spacing) ** 2 / 2)) <= max_dim:
             if d * (1 + tan_angle) > self.b:
-                x = -x_offset + d - (self.b - d) / tan_angle
-                if x >= x_offset:
+                x = -x_off + d - (self.b - d) / tan_angle
+                if x >= x_off:
                     break
                 x_list.append(x + self.x)
-                z_list.append(z_offset + self.z)
+                z_list.append(z_off + self.z)
             else:
-                x_list.append(-x_offset + self.x)
-                z_list.append(-z_offset + d * (1 + tan_angle) + self.z)
+                x_list.append(-x_off + self.x)
+                z_list.append(-z_off + d * (1 + tan_angle) + self.z)
 
             if d * (1 + 1 / tan_angle) < self.a:
-                x_list.append(-x_offset + d * (1 + 1 / tan_angle) + self.x)
-                z_list.append(-z_offset + self.z)
+                x_list.append(-x_off + d * (1 + 1 / tan_angle) + self.x)
+                z_list.append(-z_off + self.z)
             else:
-                z = -z_offset + d - (self.a - d) * tan_angle
-                x_list.append(x_offset + self.x)
+                z = -z_off + d - (self.a - d) * tan_angle
+                x_list.append(x_off + self.x)
                 z_list.append(z + self.z)
             n += 1
-        x, z = rotate(
+        x, z = transform(
             self.x, self.z,
             np.array(x_list), np.array(z_list),
-            rotation=self.rotation
+            self.rotation, self.scale
         )
         hatching = [
             go.Scatter(x=[x[i], x[i + 1]], y=[z[i], z[i + 1]],
