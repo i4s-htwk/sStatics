14e6ec35b80bfe64605b9049ce0324d08c50b41c
Flo <florian.heinrich@htwk-leipzig.de>
2025-07-31 11:10:04 +0200

    docs: add NumPy-style docstrings to Mesh class

--- sstatics/core/preprocessing/system.py
+++ sstatics/core/preprocessing/system.py
@@ -133,6 +133,20 @@ class System:
 
 @dataclass(eq=False)
 class Mesh:
+    r"""Represents a mesh of bars.
+
+    Attributes
+    ----------
+    bars : List[Bar]
+        A list of bars to be meshed.
+    user_divisions : Optional[Dict[Bar, List[float]]]
+        Optional user-defined divisions for each bar.
+
+    Notes
+    -----
+    The `user_divisions` dictionary maps each bar to a list of positions
+    where the bar should be divided.
+    """
     bars: List[Bar]
     user_divisions: Optional[Dict[Bar, List[float]]] = None
 
@@ -146,14 +160,35 @@ class Mesh:
 
     @property
     def mesh(self):
+        r"""Gets the generated mesh.
+
+        Returns
+        -------
+        List[Bar]
+            The list of bars in the mesh.
+        """
         return self._mesh
 
     @property
     def user(self):
+        r"""Gets the user-defined mesh.
+
+        Returns
+        -------
+        List[Bar]
+            The list of bars in the user-defined mesh.
+        """
         return self._user
 
     @property
     def _map_mesh_bar(self):
+        r"""Gets a mapping from mesh bars to their original bars.
+
+        Returns
+        -------
+        Dict[Bar, Bar]
+            A dictionary mapping each mesh bar to its original bar.
+        """
         reverse_map = {}
         for bar, mesh in self._map_bar_mesh.items():
             for segment in mesh:
@@ -161,6 +196,19 @@ class Mesh:
         return reverse_map
 
     def generate(self):
+        r"""Generates the mesh based on the input bars and user divisions.
+
+        This method populates the `mesh` and `user` properties.
+
+        Returns
+        -------
+        None
+
+        Notes
+        -----
+        The mesh generation process involves splitting the input bars into
+        segments based on user-defined divisions and point loads.
+        """
         user_divisions = self.user_divisions or {}
         calc_mesh = []
         user_mesh = []
@@ -210,16 +258,72 @@ class Mesh:
         self._map_bar_mesh = map_bar_mesh
 
     def mesh_segments_of(self, bar: Bar) -> List[Bar]:
+        r"""Gets the mesh segments corresponding to a given bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The bar for which to retrieve mesh segments.
+
+        Returns
+        -------
+        List[Bar]
+            The list of mesh segments corresponding to the given bar.
+        """
         return self._map_bar_mesh.get(bar)
 
     def user_segments_of(self, bar: Bar) -> List[Bar]:
+        r"""Gets the user-defined segments corresponding to a given bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The bar for which to retrieve user-defined segments.
+
+        Returns
+        -------
+        List[Bar]
+            The list of user-defined segments corresponding to the given bar.
+        """
         return self._map_bar_user.get(bar)
 
     def bar_of(self, mesh_segment: Bar) -> Bar:
+        r"""Gets the original bar corresponding to a given mesh segment.
+
+        Parameters
+        ----------
+        mesh_segment : Bar
+            The mesh segment for which to retrieve the original bar.
+
+        Returns
+        -------
+        Bar
+            The original bar corresponding to the given mesh segment.
+        """
         return self._map_mesh_bar.get(mesh_segment)
 
     @staticmethod
     def _assign_loads_to_segments(load_pos_i, user_pos_i):
+        r"""Assigns loads to segments based on their positions.
+
+        Parameters
+        ----------
+        load_pos_i : Dict[float, List[NodePointLoad]]
+            A dictionary of loads at specific positions.
+        user_pos_i : List[float]
+            A list of user-defined positions.
+
+        Returns
+        -------
+        Dict[int, Dict[float, List[NodePointLoad]]]
+            A dictionary mapping segment indices to loads at specific
+            positions.
+
+        Notes
+        -----
+        The loads are assigned to segments based on their positions relative to
+        the user-defined positions.
+        """
         user_pos_i = sorted(set(user_pos_i) | {0.0, 1.0})
         index = {}
 
@@ -238,10 +342,37 @@ class Mesh:
 
     @staticmethod
     def _get_point_loads(bar):
+        r"""Gets the point loads on a given bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The bar for which to retrieve point loads.
+
+        Returns
+        -------
+        Dict[float, List[NodePointLoad]]
+            A dictionary mapping positions to lists of point loads.
+        """
         return {load.position: [load] for load in bar.point_loads}
 
     @staticmethod
     def _transfer_loads_to_user_pos(user_pos_i: list[float], load_pos_i):
+        r"""Transfers loads to user-defined positions.
+
+        Parameters
+        ----------
+        user_pos_i : List[float]
+            A list of user-defined positions.
+        load_pos_i : Dict[float, List[NodePointLoad]]
+            A dictionary of loads at specific positions.
+
+        Returns
+        -------
+        Tuple[Dict[float, List[NodePointLoad]], Dict[float,
+            List[NodePointLoad]]]
+            A tuple containing the updated user positions and remaining loads.
+        """
         user_positions = defaultdict(list, {k: [] for k in user_pos_i})
         for pos in list(load_pos_i):
             if pos in user_positions or pos in (0.0, 1.0):
@@ -252,6 +383,20 @@ class Mesh:
 
     @staticmethod
     def _add_end_loads(user_positions, bar):
+        r"""Adds end loads to user-defined positions.
+
+        Parameters
+        ----------
+        user_positions : Dict[float, List[NodePointLoad]]
+            A dictionary of user-defined positions and user loads.
+        bar : Bar
+            The bar for which to add end loads.
+
+        Returns
+        -------
+        Dict[float, List[NodePointLoad]]
+            The updated dictionary of user-defined positions and loads.
+        """
         for load in bar.point_loads:
             if load.position in (0.0, 1.0):
                 user_positions[load.position].append(load)
@@ -259,6 +404,18 @@ class Mesh:
 
     @staticmethod
     def _to_node_load(point_loads):
+        r"""Converts point loads to node loads.
+
+        Parameters
+        ----------
+        point_loads : List[NodePointLoad]
+            A list of point loads.
+
+        Returns
+        -------
+        List[NodePointLoad]
+            The list of node loads.
+        """
         return [
             NodePointLoad(load.x, load.z, load.phi, load.rotation)
             for load in point_loads
@@ -266,6 +423,20 @@ class Mesh:
 
     @staticmethod
     def _interp_coords(bar, position: float):
+        r"""Interpolates coordinates at a given position on a bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The bar for which to interpolate coordinates.
+        position : float
+            The position on the bar.
+
+        Returns
+        -------
+        Tuple[float, float]
+            The interpolated x and z coordinates.
+        """
         c, s = np.cos(bar.inclination), np.sin(bar.inclination)
         return (
             bar.node_i.x + c * position * bar.length,
@@ -276,6 +447,22 @@ class Mesh:
     def _interp_lloads(bar: Bar,
                        prev_bar: Optional[Bar],
                        pos: Optional[float] = None):
+        r"""Interpolates line loads on a bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The bar for which to interpolate line loads.
+        prev_bar : Optional[Bar]
+            The previous bar (if any).
+        pos : Optional[float]
+            The position on the bar (if any).
+
+        Returns
+        -------
+        List[LineLoad]
+            The list of interpolated line loads.
+        """
         if not bar.line_loads:
             return []
         return [
@@ -291,6 +478,21 @@ class Mesh:
     def _split(self,
                bar: Bar,
                pos_dict: Dict[float, List[NodePointLoad]]) -> List[Bar]:
+        r"""Splits a bar into segments based on user-defined positions and
+        loads.
+
+        Parameters
+        ----------
+        bar : Bar
+            The bar to be split.
+        pos_dict : Dict[float, List[NodePointLoad]]
+            A dictionary of positions and loads.
+
+        Returns
+        -------
+        List[Bar]
+            The list of split bars.
+        """
         bars = []
         prev_bar = None
 
@@ -313,10 +515,44 @@ class Mesh:
         return bars
 
     def _node(self, bar, position: float, point_loads: List[NodePointLoad]):
+        r"""Creates a new node at a given position on a bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The bar on which to create the node.
+        position : float
+            The position on the bar.
+        point_loads : List[NodePointLoad]
+            The point loads at the node.
+
+        Returns
+        -------
+        Node
+            The created node.
+        """
         x, z = self._interp_coords(bar, position)
         return Node(x, z, loads=self._to_node_load(point_loads))
 
     def _bar_first(self, bar, node_j, pos, end_point_loads):
+        r"""Creates the first segment of a bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The original bar.
+        node_j : Node
+            The end node of the segment.
+        pos : float
+            The position of the end node.
+        end_point_loads : List[BarPointLoad]
+            The point loads at the end node.
+
+        Returns
+        -------
+        Bar
+            The created bar segment.
+        """
         return replace(
             bar,
             node_i=bar.node_i,
@@ -329,6 +565,24 @@ class Mesh:
         )
 
     def _bar_middle(self, bar, prev_bar, node_j, pos):
+        r"""Creates a middle segment of a bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The original bar.
+        prev_bar : Bar
+            The previous bar segment.
+        node_j : Node
+            The end node of the segment.
+        pos : float
+            The position of the end node.
+
+        Returns
+        -------
+        Bar
+            The created bar segment.
+        """
         return replace(
             bar,
             node_i=prev_bar.node_j,
@@ -346,6 +600,22 @@ class Mesh:
         )
 
     def _bar_last(self, bar, prev_bar, end_point_loads):
+        r"""Creates the last segment of a bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The original bar.
+        prev_bar : Bar
+            The previous bar segment.
+        end_point_loads : List[BarPointLoad]
+            The point loads at the end node.
+
+        Returns
+        -------
+        Bar
+            The created bar segment.
+        """
         return replace(
             bar,
             node_i=prev_bar.node_j,
@@ -358,6 +628,13 @@ class Mesh:
         )
 
     def __call__(self):
+        r"""Generates the mesh and returns the mesh object.
+
+        Returns
+        -------
+        Mesh
+            The mesh object.
+        """
         self.generate()
         return self
 
