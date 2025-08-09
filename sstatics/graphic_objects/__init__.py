8fb3f136964d88507c25223729a5d96da18c70a4
Clarissa <clarissa_sophie.scholle@stud.htwk-leipzig.de>
2025-08-09 12:08:37 +0200

    add new classes for graphic results

--- sstatics/graphic_objects/__init__.py
+++ sstatics/graphic_objects/__init__.py
@@ -4,7 +4,7 @@ from sstatics.graphic_objects.utils import (
 )
 from sstatics.graphic_objects.geometry import (
     PointGraphic, LineGraphic, RectangleGraphic, IsoscelesTriangleGraphic,
-    PolygonGraphic, EllipseGraphic, CircularSector
+    PolygonGraphic, EllipseGraphic, CircularSectorGraphic
 )
 from sstatics.graphic_objects.diagram import Arrow, CoordinateSystem, Hatching
 from sstatics.graphic_objects.supports import (
@@ -12,19 +12,24 @@ from sstatics.graphic_objects.supports import (
     FixedSupportWPhi, ChampedSupport
 )
 from sstatics.graphic_objects.hinges import (
-    NormalForceHinge, ShearForceHinge, MomentHinge
+    NoHinge, NormalForceHinge, ShearForceHinge, MomentHinge, CombiHinge
 )
 from sstatics.graphic_objects.node import NodeGraphic
 from sstatics.graphic_objects.cross_section import CrossSectionGraphic
 from sstatics.graphic_objects.bar import BarGraphic
 from sstatics.graphic_objects.system import SystemGraphic
+from sstatics.graphic_objects.results import (
+    BarResultGraphic, ResultGraphic, SystemResultGraphic
+)
 
 
 __all__ = [
     'Arrow',
     'BarGraphic',
+    'BarResultGraphic',
     'ChampedSupport',
-    'CircularSector',
+    'CircularSectorGraphic',
+    'CombiHinge',
     'CoordinateSystem',
     'CrossSectionGraphic',
     'EllipseGraphic',
@@ -38,15 +43,18 @@ __all__ = [
     'MomentHinge',
     'MultiGraphicObject',
     'NodeGraphic',
+    'NoHinge',
     'NormalForceHinge',
     'PointGraphic',
     'PinnedSupport',
     'PolygonGraphic',
     'RectangleGraphic',
+    'ResultGraphic',
     'RollerSupport',
     'rotate',
     'ShearForceHinge',
     'SingleGraphicObject',
     'SystemGraphic',
+    'SystemResultGraphic',
     'transform'
 ]
