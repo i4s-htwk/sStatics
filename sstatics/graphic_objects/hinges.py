63a4bc8163ef798f90b30bc6a7713d12a68eb950
Clarissa <clarissa_sophie.scholle@stud.htwk-leipzig.de>
2025-08-09 12:18:55 +0200

    refactor/feat: split scatter_ and annotation_options; make width a class attribute; add CombiHinge and NoHinge classes

--- sstatics/graphic_objects/hinges.py
+++ sstatics/graphic_objects/hinges.py
@@ -15,20 +15,34 @@ class Hinge(SingleGraphicObject, ABC):
     }
 
     def __init__(self, x, z, width, **kwargs):
-        if width <= 0:
-            raise ValueError('"width" has to be a number greater than zero.')
+        if width < 0:
+            raise ValueError(
+                '"width" has to be a number greater than or equal to zero.'
+            )
         super().__init__(x, z, **kwargs)
-        self.width = width * self.scale
+        self.width = width
 
 
-class NormalForceHinge(Hinge):
+class NoHinge(Hinge):
+    width = 0
 
-    def __init__(self, x, z, width=11/30, **kwargs):
+    def __init__(self, x, z, width=width, **kwargs):
         super().__init__(x, z, width, **kwargs)
 
     @property
     def traces(self):
-        x_off, z_off = 11 / 20, self.width / 2
+        return []
+
+
+class NormalForceHinge(Hinge):
+    width = 11 / 20
+
+    def __init__(self, x, z, width=width, **kwargs):
+        super().__init__(x, z, width, **kwargs)
+
+    @property
+    def traces(self):
+        x_off, z_off = self.width, 11 / 60
         x = np.array(
             [self.x + 3 / 4 * x_off, -1 / 4 * x_off + self.x,
              -1 / 4 * x_off + self.x, self.x + 3 / 4 * x_off]
@@ -42,8 +56,9 @@ class NormalForceHinge(Hinge):
 
 
 class ShearForceHinge(Hinge):
+    width = 11 / 80
 
-    def __init__(self, x, z, width=11/80, **kwargs):
+    def __init__(self, x, z, width=width, **kwargs):
         super().__init__(x, z, width=width, **kwargs)
 
     @property
@@ -51,14 +66,14 @@ class ShearForceHinge(Hinge):
         length, x_offset = 11 / 20, self.width / 2
         left_line = LineGraphic.from_center(
             self.x - x_offset, self.z, length,
-            rotation=np.pi / 2, **self.scatter_kwargs
+            scatter_options=self.scatter_kwargs, rotation=np.pi / 2
         )
         left_line_traces = left_line.transform_traces(
             self.x, self.z, self.rotation, self.scale
         )
         right_line = LineGraphic.from_center(
             self.x + x_offset, self.z, length,
-            rotation=np.pi / 2, **self.scatter_kwargs
+            scatter_options=self.scatter_kwargs, rotation=np.pi / 2
         )
         right_line_traces = right_line.transform_traces(
             self.x, self.z, self.rotation, self.scale
@@ -67,11 +82,70 @@ class ShearForceHinge(Hinge):
 
 
 class MomentHinge(Hinge, EllipseGraphic):
+    width = 11 / 40
 
     scatter_options = Hinge.scatter_options | {
+        'line': dict(width=2),
         'fill': 'toself',
         'fillcolor': 'white',
     }
 
-    def __init__(self, x, z, width=11/40, **kwargs):
+    def __init__(self, x, z, width=width, **kwargs):
         super().__init__(x, z, a=width / 2, width=width, **kwargs)
+
+
+class CombiHinge(SingleGraphicObject):
+
+    def __init__(self, x, z, *hinges, **kwargs):
+        hinge_types = tuple(
+            type(h) if isinstance(h, Hinge) else h
+            for h in hinges
+        )
+        if not all(
+                isinstance(h, type) and issubclass(h, Hinge)
+                for h in hinge_types
+        ):
+            raise ValueError(
+                "each hinge must be an instance or a subclass of Hinge"
+            )
+        super().__init__(x, z, **kwargs)
+        self.hinge_types = hinge_types
+
+    @property
+    def get_last_hinge(self):
+        return self.hinge_types[-1]
+
+    @staticmethod
+    def _get_factor(hinge_type, is_prev):
+        width = hinge_type.width
+        if hinge_type is NormalForceHinge:
+            factor = 3 / 4 if is_prev else 1 / 4
+            return factor * width
+        return 1 / 2 * width
+
+    @property
+    def total_width(self):
+        return sum(h.width for h in self.hinge_types) * self.scale
+
+    def _get_offset(self, i):
+        if i == 0:
+            return 0.0
+        prev, curr = self.hinge_types[i - 1], self.hinge_types[i]
+        return (
+            self._get_factor(prev, is_prev=True) +
+            self._get_factor(curr, is_prev=False)
+        )
+
+    @property
+    def traces(self):
+        traces = []
+        x = self.x
+        for i, hinge_cls in enumerate(self.hinge_types):
+            x += self._get_offset(i)
+            hinge_obj = hinge_cls(
+                x, self.z, scatter_options=self.scatter_kwargs
+            ).transform_traces(
+                self.x, self.z, self.rotation, self.scale, self.tranlation
+            )
+            traces.extend(hinge_obj)
+        return traces
