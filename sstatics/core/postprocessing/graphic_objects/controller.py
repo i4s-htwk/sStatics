d89f2716603ba93522e3ab018ac744e200ea558c
Clarissa <clarissa_sophie.scholle@stud.htwk-leipzig.de>
2025-10-28 11:38:38 +0100

    refactor: restructure controller for graphic_objects

--- sstatics/core/postprocessing/graphic_objects/controller.py
+++ sstatics/core/postprocessing/graphic_objects/controller.py
@@ -1,3 +1,4 @@
+from sympy.core.cache import cached_property
 
 from sstatics.core.preprocessing.geometry import Polygon
 from .geo.geometry import PolygonGeo
@@ -11,43 +12,69 @@ class ControllerGraphic:
             self,
             *objects,
     ):
-        for obj in objects:
-            if isinstance(obj, str) and obj not in (PLOTLY, MPL):
-                raise ValueError(
-                    f'mode in objects must be PLOTLY or MPL, got {obj!r}'
-                )
+        self._validate(objects)
         self._objects = list(objects)
         self._extra_geos = []
 
+    def figure(self, show_axis=True, show_grid=False):
+        return self._render.figure(show_axis, show_grid)
+
     def show(self, show_axis=True, show_grid=False):
-        renderer = ObjectRenderer(self.geos)
-        renderer.show(show_axis, show_grid)
+        self._render.show(show_axis, show_grid)
 
     def add_geos(self, *geos):
         self._extra_geos.append(*geos)
+
+    @cached_property
+    def _render(self):
+        return ObjectRenderer(*self.geos)
 
     @property
     def _build_geos(self):
         geos = []
         for obj in self._objects:
             if isinstance(obj, (list, tuple)):
-                for o in obj:
-                    geos.append(self._core_to_geo(o))
+                geos.append([self._core_to_geo(o) for o in obj])
+            elif isinstance(obj, str):
+                geos.append(obj)
             else:
                 geos.append(self._core_to_geo(obj))
         return geos
 
+    # TODO: better name for core
     @staticmethod
     def _core_to_geo(o):
         if isinstance(o, Polygon):
             return PolygonGeo(o)
 
-        else:
-            raise ValueError(
-                f'Invalid argument: {o!r}, expected an object from the '
-                f'classes of core, a list of them, or mode string'
-            )
+    @staticmethod
+    def _validate(objects):
+
+        def _validate_core_objects(obj):
+            if not isinstance(obj, VALID_CORE_TO_GEO_OBJECTS):
+                raise ValueError(
+                    f'Unsupported or invalid input: {obj!r}. '
+                    f'Expected a core object, a geo object, a list/tuple of '
+                    f'them, or a mode string (PLOTLY/MPL).'
+                )
+
+        for obj in objects:
+            if isinstance(obj, str) and obj not in (PLOTLY, MPL):
+                raise ValueError(
+                    f'mode in objects must be PLOTLY or MPL, got {obj!r}'
+                )
+
+            if isinstance(obj, (list, tuple)):
+                for o in obj:
+                    _validate_core_objects(o)
+            else:
+                _validate_core_objects(obj)
 
     @property
     def geos(self):
         return self._build_geos + self._extra_geos
+
+
+VALID_CORE_TO_GEO_OBJECTS = (
+    str, Polygon
+)
--- sstatics/core/postprocessing/graphic_objects/renderer/convert.py
+++ sstatics/core/postprocessing/graphic_objects/renderer/convert.py
@@ -1,3 +1,4 @@
+
 import re
 
 import matplotlib.colors as mcolors
