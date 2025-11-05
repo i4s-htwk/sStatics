aa82d8152617284e057c15ce73b2eb53a54b99a3
Paula <paula.jauch@stud.htwk-leipzig.de>
2025-11-05 16:01:15 +0100

    create class 'DGLSecond' and 'DGL'

--- sstatics/core/postprocessing/__init__.py
+++ sstatics/core/postprocessing/__init__.py
@@ -1,5 +1,7 @@
 
-from sstatics.core.postprocessing.results import BarResult, SystemResult
+from sstatics.core.postprocessing.results import (
+    BarResult, DGL, DGLSecond, SystemResult
+)
 from sstatics.core.postprocessing import calc_methods
 from sstatics.core.postprocessing.calc_methods import *  # noqa: F401, F403
 
@@ -7,5 +9,7 @@ from sstatics.core.postprocessing.calc_methods import *  # noqa: F401, F403
 __all__ = [
     'BarResult',
     'calc_methods',
+    'DGL',
+    'DGLSecond',
     'SystemResult',
 ]
