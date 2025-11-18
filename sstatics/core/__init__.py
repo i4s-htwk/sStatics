63c9b461b776e987db38fb683b8ac24b4e7ef464
Paula <paula.jauch@stud.htwk-leipzig.de>
2025-11-18 16:43:48 +0100

    add differential_equation function in FirstOrder and static_methods

--- sstatics/core/__init__.py
+++ sstatics/core/__init__.py
@@ -1,13 +1,16 @@
 
-from sstatics.core import calc_methods, postprocessing, preprocessing, solution
+from sstatics.core import (
+    calc_methods, postprocessing, preprocessing, utils, solution)
 from sstatics.core.calc_methods import *  # noqa: F401, F403
 from sstatics.core.postprocessing import *  # noqa: F401, F403
 from sstatics.core.preprocessing import *  # noqa: F401, F403
+from sstatics.core.utils import *  # noqa: F401, F403
 from sstatics.core.solution import *  # noqa: F401, F403
 
 __all__ = [
     'calc_methods',
     'postprocessing',
     'preprocessing',
+    'utils',
     'solution',
 ]
--- sstatics/core/calc_methods/first_order.py
+++ sstatics/core/calc_methods/first_order.py
@@ -1,8 +1,10 @@
 
 from dataclasses import dataclass
+from typing import Literal
 
 from sstatics.core.preprocessing.system import Bar
 from sstatics.core.solution.solver import Solver
+from sstatics.core.utils import get_differential_equation
 
 
 @dataclass(eq=False)
@@ -25,3 +27,28 @@ class FirstOrder(Solver):
                     f"All bars must be instances of `Bar`. "
                     f"Found {type(bar).__name__}."
                 )
+
+    def differential_equation(
+            self, bar_index: int | None = None, n_disc: int = 10
+    ):
+        return get_differential_equation(
+            self.system.mesh, self.bar_deform_list, self.internal_forces,
+            bar_index, n_disc
+        )
+
+    def plot(
+            self,
+            kind: Literal[
+                'normal', 'shear', 'moment', 'u', 'w', 'phi'] = 'normal',
+            bar_mesh_type: Literal['bars', 'user_mesh', 'mesh'] = 'bars',
+            result_mesh_type: Literal['bars', 'user_mesh', 'mesh'] = 'mesh',
+            decimals: int | None = None, n_disc: int = 10
+             ):
+        from sstatics.graphic_objects import ResultGraphic
+        from sstatics.core.postprocessing import SystemResult
+        result = SystemResult(
+            self.system, self.bar_deform_list, self.internal_forces,
+            self.node_deform, self.node_support_forces,
+            self.system_support_forces, n_disc=n_disc)
+        ResultGraphic(
+            result, kind, bar_mesh_type, result_mesh_type, decimals).show()
